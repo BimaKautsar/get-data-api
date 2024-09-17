2627f727d5c6cbff0d60791e42eb6f34fdcef2ee
BimaKautsar <bimakautsar01@gmail.com>
2024-09-17 16:38:12 +0700

    first commit

---
+++ components/Fragments/FormLogin.jsx
@@ -0,0 +1,35 @@
+import InputForm from "../Elements/Input";
+import Button from "../Elements/Button";
+import { useRef } from "react";
+
+const FormLogin = () => {
+  const handleLogin = (event) => {
+    event.preventDefault();
+    localStorage.setItem("email",event.target.email.value);
+    localStorage.setItem("password",event.target.password.value);
+    window.location.href = "/products";
+  };
+  const emailRef = useRef(null);
+    return (
+      <form onSubmit={handleLogin}>
+        <InputForm
+          label="Email"
+          placeholder="Example@gmail.com"
+          type="email"
+          name="email"
+          ref={emailRef}
+        />
+        <InputForm
+          label="Password"
+          name="password"
+          type="password"
+          placeholder="********"
+        />
+        <Button classname="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded w-full" type="submit">
+          Login
+        </Button>
+      </form>
+    );
+};
+
+export default FormLogin;
